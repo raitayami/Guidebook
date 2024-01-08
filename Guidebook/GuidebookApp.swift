94e0486ac7aa4b492467fa9108414a9897febc72
raitayami <66219215+raitayami@users.noreply.github.com>
2024-01-08 13:44:08 +0000

    Initial Commit

---
+++ Guidebook/GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookApp.swift
+//  Guidebook
+//
+//  Created by Tayami Rai on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
